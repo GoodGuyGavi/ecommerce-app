5b9cba9d025fa5b3e058cb99603580804836084a
GoodGuyGavi <gavilan.salcedo@gmail.com>
2021-02-17 09:36:02 +0800

    add menu item constants

---
+++ src/constants/sections.js
@@ -0,0 +1,31 @@
+const sections = [
+	{
+		id: 1,
+		title: 'Hats',
+		imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+	},
+	{
+		id: 2,
+		title: 'Sneakers',
+		imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+	},
+	{
+		id: 3,
+		title: 'Womens',
+		imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+	},
+	{
+		id: 4,
+		title: 'Mens',
+		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+		size: 'large',
+	},
+	{
+		id: 5,
+		title: 'Mens',
+		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+		size: 'large',
+	},
+]
+
+export default sections
